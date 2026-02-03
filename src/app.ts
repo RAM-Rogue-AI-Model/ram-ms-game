dc9e58673770010550e3f95c19a42851de59cd2f
Hugo CREPIN <crepin.hugo30@gmail.com>
2026-02-03 20:53:06 +0100

    open api battle

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 import 'dotenv/config';
 
+import fs from 'node:fs';
+
 import cors from 'cors';
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import * as YAML from 'yaml';
 
 import { GameController } from './controllers/gameController';
 import { GameRouter } from './routes/gameRouter';
@@ -25,7 +29,13 @@ const gameController = new GameController(gameService);
 
 app.use('/game', new GameRouter(gameController).router);
 
+const file = fs.readFileSync('./openapi.yml', 'utf8');
+const swaggerDocument = YAML.parse(file) as object;
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.listen(port, () => {
   // eslint-disable-next-line no-console
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`docs available at http://localhost:${port}/docs`);
 });
